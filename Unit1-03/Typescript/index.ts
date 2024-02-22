2e4bd1d2663f1055766d576069b5f475cf5916f7
Kenny Le <kenny.le@mths.ca>
2024-02-22 16:40:22 +0000

    init

--- Unit1-03/Typescript/index.ts
+++ Unit1-03/Typescript/index.ts
@@ -1,1 +1,27 @@
-qwerty
+/**
+ * This program calculates energy using 
+ * Einstein's equation
+ * By: Kenny Le
+ * Version: 1.0
+ * Since:   2024-02-16
+ */
+
+import { createPrompt } from 'bun-promptx'
+
+// variables
+const subTime = 1
+const pizzaTime = 0.45
+const soupTime = 1.45
+
+// input
+const foodChoice= createPrompt("Are you heating sub, pizza, or soup?: ")
+const quantityFood = createPrompt("How many " (foodchoice) "(s) are you cooking?(max:3): ")
+const acceptableWords = ['sub', 'pizza', 'soup'];
+
+if (acceptableWords.includes(userInput)) {
+    console.log("Valid input");
+} else {
+    console.log("Invalid input");
+  }
+ 
+console.log("\nDone.")
