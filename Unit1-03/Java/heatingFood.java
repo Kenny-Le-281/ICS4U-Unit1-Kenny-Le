1b85d2e555c3166c6f10e23791856378d2d0e63f
Kenny Le <kenny.le@mths.ca>
2024-02-22 23:30:40 +0000

    ts and java

---
+++ Unit1-03/Java/heatingFood.java
@@ -0,0 +1,10 @@
+/*
+* This program calculates the energy an object can release
+*
+* @author  Kenny Le
+* @version 1.0
+* @since   2024-02-21
+*/
+
+import java.util.Scanner;
+
--- Unit1-03/Typescript/index.ts
+++ Unit1-03/Typescript/index.ts
@@ -1,27 +1,57 @@
 /**
- * This program calculates energy using 
- * Einstein's equation
+ *
+ * This program checks time for pizza, sub, soup
+ *
  * By: Kenny Le
- * Version: 1.0
- * Since:   2024-02-16
+ * Version 1.0
+ * Since 2024-02-21
  */
 
-import { createPrompt } from 'bun-promptx'
+import { createSelection } from 'bun-promptx'
 
-// variables
-const subTime = 1
-const pizzaTime = 0.45
-const soupTime = 1.45
+const foodSelectedInt = createSelection([
+  { text: 'sub' },
+  { text: 'pizza' },
+  { text: 'soup' }
+], {
+  headerText: 'Select food to microwave: ',
+  perPage: 3
+})
 
-// input
-const foodChoice= createPrompt("Are you heating sub, pizza, or soup?: ")
-const quantityFood = createPrompt("How many " (foodchoice) "(s) are you cooking?(max:3): ")
-const acceptableWords = ['sub', 'pizza', 'soup'];
+// Input amount of food
+const foodAmountInt = createSelection([
+  { text: 'One' },
+  { text: 'Two' },
+  { text: 'Three' }
+], {
+  headerText: 'Select amount: ',
+  perPage: 3
+})
 
-if (acceptableWords.includes(userInput)) {
-    console.log("Valid input");
-} else {
-    console.log("Invalid input");
-  }
- 
-console.log("\nDone.")
+let foodString = "nothing"
+let foodTimeTotal = 0
+switch (foodSelectedInt.selectedIndex) {
+case 0:
+  foodString = "sub"
+  foodTimeTotal = 60
+  break
+case 1:
+  foodString = "pizza"
+  foodTimeTotal = 45
+  break
+case 2:
+  foodString = "soup"
+  foodTimeTotal = 105
+  break
+}
+
+foodTimeTotal = foodTimeTotal * (1 + (0.5 * foodAmountInt.selectedIndex))
+const foodAmount = foodAmountInt.selectedIndex + 1
+const foodTimeMinutes = Math.floor(foodTimeTotal / 60)
+const foodTimeSeconds = foodTimeTotal - (60 * foodTimeMinutes)
+
+console.log(`Total cook time for ${foodAmount} ${foodString}(s) is ${foodTimeMinutes} minute(s) and ${foodTimeSeconds} seconds.`)
+
+// Show the program as done
+console.log('\nDone.')
+
