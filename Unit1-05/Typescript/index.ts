d949c7a759879f81cb24337a072670323a5a9a2f
Kenny Le <kenny.le@mths.ca>
2024-03-02 03:38:53 +0000

    ts

--- Unit1-05/Typescript/index.ts
+++ Unit1-05/Typescript/index.ts
@@ -8,30 +8,30 @@
 
 import { createPrompt } from 'bun-promptx'
 
-function checkBoardFoot(width, number) {
-    const boardFoot = 144
-    const lengthInInches = boardFoot / (width * height);
-    return lengthInInches;
+function checkBoardFoot(width, height) {
+  const boardFoot = 144
+  const lengthInInches = boardFoot / (width * height);
+  return lengthInInches;
 }
 
 console.log("This program figures out the length of a wooden board that is 1 board foot in dimensions. (1 board foot is 144 cubic inches of wood.)")
 while (true) { 
-    const widthString = prompt('Enter the width(inch): ')
-    const width = parseFloat(widthString)
-    const heightString = prompt('Enter the height(inch): ')
-    const height = parseFloat(heightString)
-    if (isNaN(width) || isNaN(height)) {
-        console.log('Invalid Input')
-        break
-    } else if (width < 0 || height < 0) {
-        console.log('Invalid Input')
-        break
-    } else {
-        const lengthInInches = BoardFoot(width, height)
-        console.log('\nLength of the board:', lengthInInches, 'inch(es) long.')
-        break
-    }
+  const widthString = prompt('Enter the width(inch): ')
+  const width = parseFloat(widthString)
+  const heightString = prompt('Enter the height(inch): ')
+  const height = parseFloat(heightString)
+  if (isNaN(width) || isNaN(height)) {
+    console.log('Invalid Input')
+    break
+  } else if (width < 0 || height < 0) {
+    console.log('Invalid Input')
+    break
+  } else {
+    const lengthInInches = checkBoardFoot(width, height)
+    console.log('\nLength of the board:', lengthInInches, 'inch(es) long.')
+    break
+  }
 }
-    
+ 
 console.log('\nDone.')
 
